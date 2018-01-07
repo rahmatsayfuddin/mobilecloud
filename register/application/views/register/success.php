cafcd680d9d408eb685fd6923c9ca4f69302b287
Unknown <rahmat.sayfuddin@binus.ac.id>
2018-01-07 15:02:57 +0700

    success message

--- register/application/views/register/success.php
+++ register/application/views/register/success.php
@@ -8,42 +8,10 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.6/umd/popper.min.js"></script> 
 	<script src="<?php echo base_url()?>/assets/plugin/bootstrap/js/bootstrap.min.js"></script>
 	
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/custom/bootstrapcustom.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>/assets/custom/login_register.css">
 </head>
 <style type="text/css">
-body{
-	background: url("../../assets/image/home/bg.png") no-repeat center center fixed;
-	-moz-background-size: cover;
-	-webkit-background-size: cover;
-	-o-background-size: cover;
-	background-size: cover;
-}
-.logo-center{
-	display: block;
-	margin-top: 2%;
-	margin-bottom: 2%;
-	margin-left: auto;
-	margin-right: auto;
-	width: 35%;
-}
-.login-box{
-	float: none;
-	display: block;
-	margin-left: auto;
-	margin-right: auto;
-	background: #F1EFF2;
-	border:solid 0.5px #D01800;
-	margin-bottom: 2%;
-}
-.btn-danger {
-	color: #fff !important;
-	background-color:#D01800;
-	border-color: #D01800;
-}
-.btn-danger:hover, .btn-danger:focus, .btn-danger:active, .btn-danger.active, .open>.dropdown-toggle.btn-danger {
-	color: #fff !important;
-	background-color: #dc3545;
-	border-color: #dc3545;
-}
 .btn-selesei{
 	display: table;
 	margin-left: auto;
@@ -52,15 +20,15 @@ body{
 }
 </style>
 <body>
-	<img src="<?php echo base_url()?>/assets/image/home/logo_login.png"  class="logo-center">
+	<img src="<?php echo base_url()?>/assets/image/home/logo_mobile_cloud.png""  class="logo-center">
 	<div class="container">
 		<div class="col-sm-6 login-box">
 			<div class="row">
 				<div class="col-sm-12">
 					<br>
 					<h5 class="text-center"><?php echo $result['message']; ?></h5>
-					<a href="<?php echo site_url()?>/login" class="btn btn-danger btn-selesei">
-						SELESAI
+					<a href="<?php echo site_url()?>/" class="btn btn-danger btn-selesei">
+						Try Again
 					</a>
 				</div>
 			</div>
---
+++ register/application/views/success.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Mobile CLoud</title>
+</head>
+<style type="text/css">
+body{
+	margin: 0;
+	/*background: #DF0000;*/
+	background-image:url("<?php echo base_url()?>assets/image/home/bg_register.jpg");
+	background-repeat: no-repeat;
+	background-size:cover;
+}
+.background{
+	text-align: center;
+	font-family: logo;
+	color: #FFFFFF;
+}
+.content{
+	width: 100%; 
+	height: auto;
+	margin: 0 auto; 
+}
+.link{
+	width: 33.33%;
+	float: left;
+}
+.link img{
+	width: 80%
+}
+@font-face {
+	font-family: logo;
+	src: url(../../TRASHHAND.TTF);
+}
+</style>
+<body>
+	<div class="background">
+		<div class="content">
+			<h1>Congratulation !!</h1>
+			<h2>You have got access to all product of mobile cloud</h2>
+			<div class="product">
+				<div class="link">
+					<a  href="https://play.google.com/store/apps/details?id=com.telkom.telkomchats" target="_blank"> <img src="../../ic_telkomchat.png"></a>
+				</div>
+				<div class="link">
+					<a href="https://play.google.com/store/apps/details?id=com.telkomsigma.telkomstorage" target="_blank" > <img src="../../ic_telkomstorage.png"></a>
+				</div>
+				<div class="link">
+					<a href="https://play.google.com/store/apps/details?id=com.telkomsigma.telkommail" target="_blank"><img src="ic_telkommail.png"> <img src="../../ic_telkommail.png"></a>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
